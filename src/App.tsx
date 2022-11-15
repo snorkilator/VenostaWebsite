9e86df4d74d8ddc154f57743a170ad37bbd9c326
snorkilator <daniel@rudenberg.ca>
2022-11-15 17:11:17 -0500

    new build

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,6 @@ function App() {
   return (
     <div className="App">
       <button className='btn btn-primary'>bootstrap!!</button>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -25,9 +16,6 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
