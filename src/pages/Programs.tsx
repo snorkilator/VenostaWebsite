0c8237fa4435f8302de05f5179d5f6a54129e299
snorkilator <daniel@rudenberg.ca>
2022-11-23 13:18:20 -0500

    test run of MD conversion

--- src/pages/Programs.tsx
+++ src/pages/Programs.tsx
@@ -1,16 +1,23 @@
 import { useState } from "react";
 import { Remarkable } from "remarkable";
 import parse from "html-react-parser";
-import { Container } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 
 export default function Programs() {
   var md = new Remarkable();
   const test = `
-* test
-* tes2
-# test
-## test
+  # List of dependencies
+
+  * [Remarkable](https://github.com/jonschlinkert/remarkable) for md to HTML on the fly
+    * checkout config in GH
+  * react-bootstrap for bootstrap react pre-build components (can do this without this with plain bootstrap)
+  * html-react-parser for string to react html elements
+    * might be unsafe
+  * react-router-dom for browser side routing
+  * all asociated typescript files (d.ts)
+  
+  ![image](https://upload.wikimedia.org/wikipedia/commons/d/d9/Flag_of_Canada_%28Pantone%29.svg)
     `;
   const danger = md.render(test);
-  return <Container>{parse(danger)}</Container>;
+  return <Container ><Row  >{parse(danger)}</Row></Container>;
 }
