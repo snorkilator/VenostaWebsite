6d64491305c6ec96a7bff2de66fc5f4cbf06a3b8
snorkilator <daniel@rudenberg.ca>
2022-12-07 13:29:06 -0500

    built layout of about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,103 @@
 import { useState } from "react";
-import {
-} from "react-bootstrap";
+import { Container, Col, Row } from "react-bootstrap";
 
-export default function About(){
-    return <p>About</p>
+import Programs from "./Programs";
+
+export default function About() {
+  return (
+    <Container>
+      <h1 className="mission">Mission</h1>
+      <p className="mission">
+        Quod aspernatur temporibus quia. Cumque sunt qui alias possimus optio
+        vitae quos. Nostrum vel iusto porro distinctio. Possimus minus
+        voluptatibus consequatur rerum qui enim quo. Quisquam sit qui expedita
+        esse velit eius autem veniam. Facilis neque quam sint harum sit et quo
+        omnis.
+      </p>
+      <h1 className="departments">Departments</h1>
+      <Programs />
+      <h1 className="staff">Staff</h1>
+      <Row>
+        <Col sm={12} md={6} lg={4}>
+          <h5 className="Staff">Jon Doe - Coordinator</h5>
+          <p className="staff-bio">
+            Quod aspernatur temporibus quia. Cumque sunt qui alias possimus
+            optio vitae quos. Nostrum vel iusto porro distinctio. Possimus minus
+            voluptatibus consequatur rerum qui enim quo. Quisquam sit qui
+            expedita esse velit eius autem veniam. Facilis neque quam sint harum
+            sit et quo
+          </p>
+        </Col>
+        <Col sm={12} md={6} lg={4}>
+          <h5 className="Staff">Jon Doe - Coordinator</h5>
+          <p className="staff-bio">
+            Quod aspernatur temporibus quia. Cumque sunt qui alias possimus
+            optio vitae quos. Nostrum vel iusto porro distinctio. Possimus minus
+            voluptatibus consequatur rerum qui enim quo. Quisquam sit qui
+            expedita esse velit eius autem veniam. Facilis neque quam sint harum
+            sit et quo
+          </p>
+        </Col>
+        <Col sm={12} md={6} lg={4}>
+          <h5 className="Staff">Jon Doe - Coordinator</h5>
+          <p className="staff-bio">
+            Quod aspernatur temporibus quia. Cumque sunt qui alias possimus
+            optio vitae quos. Nostrum vel iusto porro distinctio. Possimus minus
+            voluptatibus consequatur rerum qui enim quo. Quisquam sit qui
+            expedita esse velit eius autem veniam. Facilis neque quam sint harum
+            sit et quo
+          </p>
+        </Col>
+        <Col sm={12} md={6} lg={4}>
+          <h5 className="Staff">Jon Doe - Coordinator</h5>
+          <p className="staff-bio">
+            Quod aspernatur temporibus quia. Cumque sunt qui alias possimus
+            optio vitae quos. Nostrum vel iusto porro distinctio. Possimus minus
+            voluptatibus consequatur rerum qui enim quo. Quisquam sit qui
+            expedita esse velit eius autem veniam. Facilis neque quam sint harum
+            sit et quo
+          </p>
+        </Col>
+        <Col sm={12} md={6} lg={4}>
+          <h5 className="Staff">Jon Doe - Coordinator</h5>
+          <p className="staff-bio">
+            Quod aspernatur temporibus quia. Cumque sunt qui alias possimus
+            optio vitae quos. Nostrum vel iusto porro distinctio. Possimus minus
+            voluptatibus consequatur rerum qui enim quo. Quisquam sit qui
+            expedita esse velit eius autem veniam. Facilis neque quam sint harum
+            sit et quo
+          </p>
+        </Col>
+      </Row>
+      <h1 className="history">History</h1>
+      <p className="history">
+        Quod aspernatur temporibus quia. Cumque sunt qui alias possimus optio
+        vitae quos. Nostrum vel iusto porro distinctio. Possimus minus
+        voluptatibus consequatur rerum qui enim quo. Quisquam sit qui expedita
+        esse velit eius autem veniam. Facilis neque quam sint harum sit et quo
+        omnis.
+      </p>
+      <h1 className="board">Board</h1>
+      <Row>
+        <Col>Treasurer - John Doe</Col>
+        <Col>Treasurer - John Doe</Col>
+      </Row>
+      <Row>
+        <Col>Treasurer - John Doe</Col>
+        <Col>Treasurer - John Doe</Col>
+      </Row>
+      <Row>
+        <Col>Treasurer - John Doe</Col>
+        <Col>Treasurer - John Doe</Col>
+      </Row>
+      <Row>
+        <Col>Treasurer - John Doe</Col>
+        <Col>Treasurer - John Doe</Col>
+      </Row>
+      <Row>
+        <Col>Treasurer - John Doe</Col>
+        <Col>Treasurer - John Doe</Col>
+      </Row>
+    </Container>
+  );
 }
